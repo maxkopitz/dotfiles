83b912bba0e6eeae277cc3b2cfe3ad3749e17817
Max Kopitz <maxkopitz02@gmail.com>
2023-03-14 12:39:24 -0400

    feat(install)

--- bin/config_install.sh
+++ bin/config_install.sh
@@ -14,4 +14,12 @@ install_macos() {
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
-
+install_oh_my_zsh() {
+  if [[ ! -f ~/.zshrc ]]; then
+    info "Installing oh my zsh..."
+    ZSH=~/.oh-my-zsh ZSH_DISABLE_COMPFIX=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
+  else
+    warn "oh-my-zsh already installed"
+  fi
+}
