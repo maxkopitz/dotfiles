be0735093b77876a0a16689789e671bcf126acc3
Maxwell Kopitz <maxwellkopitz@Maxwells-MacBook-Pro.local>
2023-03-14 12:23:35 -0400

    installed on new mac

--- bin/config_install.sh
+++ bin/config_install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 set -exu
 set -o pipefail
+
+
+install_brew() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+install_macos() {
+  brew tap homebrew/cask-fonts
+  brew install font-hack-nerd-font
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/softmoth/zsh-vim-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
+
+
---
+++ bin/stow_install.sh
@@ -0,0 +1,3 @@
+stow_dotfiles() {
+  
+}
